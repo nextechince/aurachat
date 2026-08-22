9c92318334b60894b039278bd44ad1e5f0a7edf3
nextechince <nextechince@gmail.com>
2026-08-22 17:37:41 +0300

    Update security_screen.dart

--- lib/screens/settings/security_screen.dart
+++ lib/screens/settings/security_screen.dart
@@ -3,7 +3,6 @@ import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:local_auth/local_auth.dart';
 import 'package:local_auth_android/local_auth_android.dart';
-import 'package:local_auth_ios/local_auth_ios.dart';
 import '../../providers/settings_provider.dart';
 
 class SecurityScreen extends StatefulWidget {
