7dfb36f5e24cb289ecaf6b870490e7acca01347e
nextechince <nextechince@users.noreply.github.com>
2026-09-07 21:26:52 +0000

    deploy: 0e566894be2071f2d985ba2d7a9672d505495817

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f6344b75dcf861d8bf1f1322780b8811f982e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "918989828"
+    serviceWorkerVersion: "3119146480"
   }
 });
